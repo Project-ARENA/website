63228f034a581c5411a66900ad9fe337dab44399
Super Sayf <sayfullah.jumoorty@live.com>
2023-03-16 19:29:35 +0200

    PA-3 Updated front-end

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './style.css'
+import Contact from './views/contact'
+import Register from './views/register'
+import TEST from './views/test'
 import Home from './views/home'
+import Login from './views/login'
+import Competitions from './views/competitions'
 
 const App = () => {
   return (
     <Router>
       <div>
+        <Route component={Contact} exact path="/contact" />
+        <Route component={Register} exact path="/register1" />
+        <Route component={TEST} exact path="/test" />
         <Route component={Home} exact path="/" />
+        <Route component={Login} exact path="/login" />
+        <Route component={Competitions} exact path="/competitions" />
       </div>
     </Router>
   )
