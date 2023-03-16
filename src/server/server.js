a7a3d71bbe8cde5610714c558d0cc442cafc7984
Daggy <momosuli8@gmail.com>
2023-03-16 14:53:31 +0200

    Backend starting
    Added route to put registration details in table

--- src/server/server.js
+++ src/server/server.js
@@ -23,6 +23,29 @@ db.query("SELECT * FROM users", (err,result)=>{
 res.send(result)
 });   });
 
+//Route to get registration details and insert
+app.post('/daggy', (req,res)=> {
+
+    const name = req.body.name;
+    const surname = req.body.surname;
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+
+    
+    db.query("INSERT INTO users (user_firstname, user_surname, user_nickname, user_password, user_email, user_admin) VALUES (?,?,?,?,?,?)",[name, surname,username,password,email,0], (err,result)=>{
+       if(err) {
+        res.send(err)
+       console.log(err)
+       } 
+       console.log(result)
+    });   
+})
+
+
+
+//Type above this
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
 })
+
