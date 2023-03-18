9eb863541aa3b803be82bf06b71a18da39129b5c
Sayfullah <sayfullah.jumoorty@live.com>
2023-03-18 16:36:52 +0200

    PA-53 Fixed button click

--- src/components/button.js
+++ src/components/button.js
@@ -1,25 +1,26 @@
-import React from 'react'
-
-import PropTypes from 'prop-types'
-
-import './button.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './button.css';
 
 const Button = (props) => {
   return (
-    <div className={`button-container ${props.rootClassName} `}>
-      <button className="button-button button">{props.button}</button>
+    <div className={`button-container ${props.rootClassName}`}>
+      <button className="button-button button" onClick={props.onClick}>
+        {props.button}
+      </button>
     </div>
-  )
-}
+  );
+};
 
 Button.defaultProps = {
   button: 'LOGIN',
   rootClassName: '',
-}
+};
 
 Button.propTypes = {
   button: PropTypes.string,
   rootClassName: PropTypes.string,
-}
+  onClick: PropTypes.func.isRequired,
+};
 
-export default Button
+export default Button;
