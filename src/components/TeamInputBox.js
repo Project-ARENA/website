d4f8f3d9d3837dd0cbfcd0af9ac93ffad925d51a
Mu'aaz Bassa <muaazbassa@gmail.com>
2023-03-27 14:37:07 +0200

    PA-69 Added UI to Create and Join team

---
+++ src/components/TeamInputBox.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Button from '../components/button'
+import InputBoxForInfo from "./input-box-for-info";
+import "./TeamInputBox.css";
+
+export default function TeamInputBox(props) {
+  const { rootClassName, title, label, buttonText , name, onClick} = props;
+  return (
+    <div className={`team-input-box ${rootClassName} `}>
+      <div className="team-input-box-container">
+        <div className="team-input-box-container1">
+            <span className="team-input-box-text">{title}</span>
+        </div>
+        <h1 className="team-input-box-text1">{label}</h1>
+        <div className = "Sayf">
+        <InputBoxForInfo buttonText={buttonText}></InputBoxForInfo>
+        <Button className="team-input-box-button"
+          name={name}
+          onClick={onClick}
+        ></Button>
+        </div>
+      </div>
+    </div>
+  );
+}
