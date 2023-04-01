4b419c2a99e926ccbe6c6cb1ac7be4ed8a4e63f7
Hamdullah Dadabhoy <abdullahkarolia47@gmail.com>
2023-04-01 16:34:10 +0200

    PA-37
    Added coverage to .gitignore file
    home page test cases

--- src/views/about.test.jsx
+++ src/views/about.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import About from "./about";
 
 describe("About", () => {
