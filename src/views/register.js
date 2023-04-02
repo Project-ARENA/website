aba895a152966b1d1ea8a79f51da9932ab26a7c6
Super Sayf <sayfullah.jumoorty@live.com>
2023-04-02 20:00:59 +0200

    Here you go @muaaz

--- src/views/register.js
+++ src/views/register.js
@@ -13,64 +13,57 @@ import InputBoxForInfo from "../components/input-box-for-info";
 import Button from "../components/button";
 import "./register.css";
 
-
-const Register = (props) => {
-  const [name, setName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+function doRegister(name, surname, email, username, password, setErrorMessage) {
   const salt = bycrypt.genSaltSync(10);
-  const hashedPassword = bycrypt.hashSync(password,salt);
-  const[exist, setExistence] = useState("");
-  const [errorMessage, setErrorMessage] = useState('');
-  const postDetails = () =>{
-    axios.post("http://localhost:3002/api/post/register",{name:name, surname:surname, email: email, username: username, password: hashedPassword});
-    setTimeout(function(){
-    window.location.href = 'http://localhost:3000/login';
+  const hashedPassword = bycrypt.hashSync(password, salt);
+
+  const postDetails = () => {
+    axios.post("http://localhost:3002/api/post/register", { name: name, surname: surname, email: email, username: username, password: hashedPassword });
+    setTimeout(function () {
+      window.location.href = 'http://localhost:3000/login';
     }, 1000);
-    
+
   }
 
-  const checkIfUserExists = () =>{
+  const checkIfUserExists = () => {
     axios
-    .get("http://localhost:3002/api/get/doesExist/" + username)
-    .then(function(response){
-      const userExists = response.data;
-      console.log(response.data);
-      if (JSON.stringify(userExists) == "[]"){
-        setErrorMessage('Account created successfully');
-        postDetails();
-      }
-      else{
-        setErrorMessage('Username already exists');
-      }
-    });
+      .get("http://localhost:3002/api/get/doesExist/" + username)
+      .then(function (response) {
+        const userExists = response.data;
+        console.log(response.data);
+        if (JSON.stringify(userExists) == "[]") {
+          setErrorMessage('Account created successfully');
+          postDetails();
+        }
+        else {
+          setErrorMessage('Username already exists');
+        }
+      });
   }
 
-  
-   const checkIfBlank=()=>{
+
+  const checkIfBlank = () => {
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 
     //If fields empty, warn
-    if(name == "" || surname =="" || email == "" || username == "" || password==""){
+    if (name == "" || surname == "" || email == "" || username == "" || password == "") {
       alert("Please enter all details");
     }
 
     //not empty
-    else{
+    else {
       //check if email correct and password is strong
-      if(emailPattern.test(email) && passwordPattern.test(password)){
+      if (emailPattern.test(email) && passwordPattern.test(password)) {
         checkIfUserExists();
       }
       //otherwise warn
-      else{
+      else {
         //email not corect format
-        if(!emailPattern.test(email)){
+        if (!emailPattern.test(email)) {
           setErrorMessage('Please enter a valid email');
         }
-        if(!passwordPattern.test(password)){
+        if (!passwordPattern.test(password)) {
           setErrorMessage("Please enter a stronger password");
         }
       }
@@ -84,7 +77,18 @@ const Register = (props) => {
     );
     checkIfBlank();
   };
-  
+
+  handleRegister();
+}
+
+const Register = (props) => {
+  const [errorMessage, setErrorMessage] = useState('');
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [exist, setExistence] = useState("");
 
   return (
     <div className="register-container">
@@ -119,36 +123,36 @@ const Register = (props) => {
           onChange={(e) => setSurname(e.target.value)}
           rootClassName="input-box-for-info-root-class-name2"
         ></InputBoxForInfo>
-    
+
         <InputBoxForInfo
           buttonText="EMAIL"
           onChange={(e) => setEmail(e.target.value)}
           rootClassName="input-box-for-info-root-class-name3"
         ></InputBoxForInfo>
-    
+
         <InputBoxForInfo
           buttonText="USERNAME"
           onChange={(e) => setUsername(e.target.value)}
           rootClassName="input-box-for-info-root-class-name4"
         ></InputBoxForInfo>
-    
+
         <InputBoxForInfo
           buttonText="PASSWORD"
           onChange={(e) => setPassword(e.target.value)}
           isPassword
           rootClassName="input-box-for-info-root-class-name5"
         ></InputBoxForInfo>
-    
+
         <br></br>
-        <Button 
+        <Button
           name="Register"
           onClick={() => {
             console.log("Register button clicked");
-            handleRegister();
+            doRegister(name, surname, email, username, password, setErrorMessage);
           }}
           rootClassName="button-root-class-name"
         ></Button>
-        
+
         <br></br>
         {errorMessage && <div className="error">{errorMessage}</div>}
         <span className="register-text3">Already have an account?</span>
