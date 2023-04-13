51b872b80ff69005779acbe1b389c0dc6a5ec2e6
Mu'aaz Bassa <muaazbassa@gmail.com>
2023-04-13 17:36:06 +0200

    PA-119 Testcase for Tabs Component
    3 Tests done

---
+++ src/components/tabs.test.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import BasicTabs from './tabs';
+
+describe('BasicTabs', () => {
+  const onSubmitMock = jest.fn();
+
+    //The component renders the correct number of tabs based on the tabCount prop.
+  test('renders correct number of tabs', () => {
+    const tabCount = 3;
+    const tabContent = [
+      <div key="tab1">Tab 1 Content</div>,
+      <div key="tab2">Tab 2 Content</div>,
+      <div key="tab3">Tab 3 Content</div>,
+    ];
+
+    const { getAllByRole } = render(
+      <BasicTabs
+        onSubmit={onSubmitMock}
+        tabCount={tabCount}
+        tabContent={tabContent}
+      />
+    );
+
+    const tabs = getAllByRole('tab');
+    expect(tabs.length).toBe(tabCount);
+  });
+
+  // The component renders the correct content based on the tabContent prop.
+  // Clicking a tab shows the corresponding content from the tabContent prop.
+  test('renders correct content when tab is clicked', () => {
+    const tabCount = 2;
+    const tabContent = [
+      <div key="tab1">Tab 1 Content</div>,
+      <div key="tab2">Tab 2 Content</div>,
+    ];
+
+    const { getByText } = render(
+      <BasicTabs
+        onSubmit={onSubmitMock}
+        tabCount={tabCount}
+        tabContent={tabContent}
+      />
+    );
+
+    fireEvent.click(getByText('Testcase 2'));
+    expect(getByText('Tab 2 Content')).toBeInTheDocument();
+
+    fireEvent.click(getByText('Testcase 1'));
+    expect(getByText('Tab 1 Content')).toBeInTheDocument();
+  });
+
+  // The component calls the onSubmit prop when the submit button is clicked. 
+  // Clicking the submit button in a tab calls the onSubmit prop with the index of the current tab.
+  test('calls onSubmit prop when submit button is clicked', () => {
+    const tabCount = 1;
+    const tabContent = [
+      <div key="tab1">Tab 1 Content</div>,
+    ];
+
+    const { getByRole } = render(
+      <BasicTabs
+        onSubmit={onSubmitMock}
+        tabCount={tabCount}
+        tabContent={tabContent}
+      />
+    );
+
+    fireEvent.click(getByRole('button'));
+    expect(onSubmitMock).toHaveBeenCalledTimes(1);
+    expect(onSubmitMock).toHaveBeenCalledWith(0); // should call with index of current tab (0 in this case)
+  });
+});
