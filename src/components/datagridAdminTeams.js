64e56c6db36e4ea0cadd346e6c19249254ff4388
Super Sayf <sayfullah.jumoorty@live.com>
2023-04-13 23:09:19 +0200

    PA-141 Pulled relevant info into textboxes

--- src/components/datagridAdminTeams.js
+++ src/components/datagridAdminTeams.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-// import { Button } from "@mui/material";
+import InputBoxForInfo from "../components/input-box-for-info";
 import { DataGrid } from '@mui/x-data-grid';
 import Modal from 'react-modal';
 import Button from '../components/button'
+import { positions } from 'react-alert';
 
 export default function CustomDataGrid({ rows }) {
 
@@ -61,15 +62,67 @@ export default function CustomDataGrid({ rows }) {
 
             <Modal isOpen={visible} style={{ content: { width: '70%', height: '70%', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }, overlay: { zIndex: 1000 } }} >
 
-                <h1>Row selected for edit: {clickedRowEdit ? `${clickedRowEdit.team_code}` : null}</h1>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'column',
+                    height: '100%'
+                }}>
 
-                <Button
-                    name="Close"
-                    onClick={() => {
-                        setvisible(false)
-                        console.log("button clicked");
-                    }}
-                />
+                    <h1>Row selected for edit:</h1>
+
+                    <InputBoxForInfo
+                        buttonText="Team Code"
+                        initialValue={clickedRowEdit ? `${clickedRowEdit.team_code}` : null}
+                        onChange={(e) => setCompname(e.target.value)}
+                    />
+
+                    <InputBoxForInfo
+                        buttonText="User ID"
+                        initialValue={clickedRowEdit ? `${clickedRowEdit.user_id}` : null}
+                        onChange={(e) => setCompname(e.target.value)}
+                    />
+
+                    <InputBoxForInfo
+                        buttonText="Team Name"
+                        initialValue={clickedRowEdit ? `${clickedRowEdit.team_name}` : null}
+                        onChange={(e) => setCompname(e.target.value)}
+                    />
+
+                    <InputBoxForInfo
+                        buttonText="Team Score"
+                        initialValue={clickedRowEdit ? `${clickedRowEdit.team_score}` : null}
+                        onChange={(e) => setCompname(e.target.value)}
+                    />
+
+                    <InputBoxForInfo
+                        buttonText="Competition Name"
+                        initialValue={clickedRowEdit ? `${clickedRowEdit.competition_name}` : null}
+                        onChange={(e) => setCompname(e.target.value)}
+                    />
+
+                    <div style={{ marginLeft: 6, marginBottom: 10, marginTop: 5 }}>
+                        <Button
+                            name="Edit"
+                            onClick={() => {
+                                setvisible(false)
+                                console.log("Edit button clicked");
+                            }}
+                        />
+                    </div>
+
+                    <div style={{ marginLeft: 6, marginBottom: 10 }}>
+                        <Button
+                            name="Close"
+                            onClick={() => {
+                                setvisible(false)
+                                console.log("Close button clicked");
+                            }}
+                        />
+                    </div>
+
+                </div>
 
             </Modal>
 
