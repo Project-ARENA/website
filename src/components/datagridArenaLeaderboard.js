fd42bc5060b7cb73de2db904d3c0953553061c35
AltaafAlly <altaaf313@gmail.com>
2023-04-15 22:22:18 +0200

    PA - 106 added location filter in comments

--- src/components/datagridArenaLeaderboard.js
+++ src/components/datagridArenaLeaderboard.js
@@ -10,7 +10,29 @@ export default function CustomDataGrid({ rows, noTests, myTeam }) {
     const columns = [
         { field: 'team_rank', headerName: 'Rank', width: 100 },
         { field: 'team_name', headerName: 'Name', width: 200 },
-        { field: 'team_location', headerName: 'Location', width: 200 },
+        { field: 'team_location', headerName: 'Location', width: 200, 
+        // valueOptions: [
+        //     { value: 99, label: "Select One" },
+        //     { value: "Free State", label: "Free State" },
+        //     { value: "Limpopo", label: "Limpopo" },
+        //     { value: "Northern Cape", label: "Northern Cape" },
+        //     { value: "Mpumalanga", label: "Mpumalanga" },
+        //     { value: "North West" , label: "North West" },
+        //     { value: "Western Cape", label: "Western Cape" },
+        //     { value: "Eastern Cape", label: "Eastern Cape" },
+        //     { value: "Gauteng", label: "Gauteng" },
+        //     { value: "Kwa-Zulu-Natal", label: "Kwa-Zulu-Natal" }
+        //   ],
+        //   valueFormatter: ({ id: rowId, value, field, api }) => {
+        //     const colDef = api.getColumn(field);
+        //     const option = colDef.valueOptions.find(
+        //       ({ value: optionValue }) => value === optionValue
+        //     );
+        
+        //     return option ? option.label : '';
+        //   },
+        //   editable: true
+        },
         // TODO: #3 Figure out how to add no. test case columns dynamically
         ...Array.from({ length: noTests }, (_, i) => ({
             field: `test_case_${i+1}`, headerName: `Test Case ${i+1}`, width: 150
