0461cacfb27aad9067849368ecd68a375af0b08f
Mu'aaz Bassa <muaazbassa@gmail.com>
2023-04-21 00:40:08 +0200

    PA-138 Testcases for TeamInputBox

--- src/components/TeamInputBox.test.jsx
+++ src/components/TeamInputBox.test.jsx
@@ -1,45 +1,108 @@
-import { render, fireEvent} from "@testing-library/react";
-import { getByRole, getByTestId, getByText } from '@testing-library/dom';
-import '@testing-library/jest-dom/extend-expect';
+import {
+  render,
+  screen,
+  fireEvent,
+  getByPlaceholderText,
+  within,
+  waitFor,
+} from "@testing-library/react";
+import { getByRole, getByTestId, getByText } from "@testing-library/dom";
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
+
 import TeamInputBox from "./TeamInputBox";
 import InputBoxForInfo from "./input-box-for-info";
 
 describe("TeamInputBox component", () => {
-    it('renders without crashing', () => {
-        render(<TeamInputBox />);
-      });
-    
-    it('renders the correct title and label', () => {
-      const { getByText } = render(<TeamInputBox title="My Title" label="My Label" />);
-      expect(getByText('My Title')).toBeInTheDocument();
-      expect(getByText('My Label')).toBeInTheDocument();
+  it("renders without crashing", () => {
+    render(<TeamInputBox />);
+  });
+
+  it("renders the correct title and label", () => {
+    const { getByText } = render(
+      <TeamInputBox title="My Title" label="My Label" />
+    );
+    expect(getByText("My Title")).toBeInTheDocument();
+    expect(getByText("My Label")).toBeInTheDocument();
+  });
+
+  it("renders the correct title and label", () => {
+    const { getByText } = render(
+      <TeamInputBox title="My Title" label="My Label" />
+    );
+    expect(getByText("My Title")).toBeInTheDocument();
+    expect(getByText("My Label")).toBeInTheDocument();
+  });
+
+  it("disables the button when disabled prop is true", () => {
+    const { getByTestId } = render(<TeamInputBox disabled={true} />);
+    const button = getByTestId("button");
+    expect(button).toBeDisabled();
+  });
+
+  it("calls onClick function when button is clicked", () => {
+    const onClick = jest.fn();
+    const { getByTestId } = render(<TeamInputBox onClick={onClick} />);
+    const button = getByTestId("button");
+    fireEvent.click(button);
+    expect(onClick).toHaveBeenCalled();
+  });
+
+  // test("updates location state when dropdown option is selected", () => {
+  //   render(<TeamInputBox />);
+  //   // const dropdown = screen.getByLabelText("Location");
+  //   const dropdown = screen.getByTestId('location-select');
+  //   userEvent.selectOptions(dropdown, "Limpopo");
+  //   expect(dropdown).toHaveValue("Limpopo");
+  // });
+
+  test("renders all dropdown menu options", () => {
+    render(<TeamInputBox />);
+    const dropdown = screen.getByTestId("location-select");
+    const dropdownOptions = within(dropdown).findAllByRole("option");
+
+    dropdownOptions.then((options) => {
+      expect(options).toHaveLength(9);
+      expect(options[0]).toHaveTextContent("Gauteng");
+      expect(options[1]).toHaveTextContent("KwaZulu-Natal");
+      expect(options[2]).toHaveTextContent("Western Cape");
+      expect(options[3]).toHaveTextContent("Free State");
+      expect(options[4]).toHaveTextContent("North West");
+      expect(options[5]).toHaveTextContent("Eastern Cape");
+      expect(options[6]).toHaveTextContent("Limpopo");
+      expect(options[7]).toHaveTextContent("Mpumalanga");
+      expect(options[8]).toHaveTextContent("Northern Cape");
     });
+  });
 
-    it('renders the correct title and label', () => {
-        const { getByText } = render(<TeamInputBox title="My Title" label="My Label" />);
-        expect(getByText('My Title')).toBeInTheDocument();
-        expect(getByText('My Label')).toBeInTheDocument();
-    });
+  // test("displays selected location", async () => {
+  //   render(<TeamInputBox />);
+  //   const dropdown = screen.getByTestId("location-select");
+  //   userEvent.selectOptions(dropdown, "Limpopo");
 
+  //   const element = screen.getByPlaceholderText("Enter your input");
+  //   expect(element).toHaveValue("Limpopo");
+  //   // await waitFor(() =>
+  //   //   expect(screen.getByTestId("location-select")).toHaveValue("Limpopo")
+  //   // );
+  // });
 
-    // it('disables the button when disabled prop is true', () => {
-    //   const { getByText } = render(<TeamInputBox disabled={true} />);
-    //   const button = getByText('Submit');
-    //   expect(button).toBeDisabled();
-    // });
-    
+  // it('should call the onClick function with input value and location when button is clicked', () => {
+  //   const onClickMock = jest.fn();
+  //   const inputValue = 'Input Value';
+  //   const locationValue = 'Gauteng';
+  //   render(<TeamInputBox onClick={onClickMock} />);
+  //   const inputBox = screen.getByPlaceholderText('Enter your input');
+  //   const locationSelect = screen.getByLabelText('Location');
+  //   fireEvent.change(inputBox, { target: { value: inputValue } });
+  //   fireEvent.change(locationSelect, { target: { value: locationValue } });
+  //   fireEvent.click(screen.getByTestId('team-input-box-button'));
+  //   expect(onClickMock).toHaveBeenCalledWith(inputValue, locationValue);
+  // });
 
-    // it('calls onClick function when button is clicked', () => {
-    //   const onClick = jest.fn();
-    //   const { getByTestId } = render(<TeamInputBox onClick={onClick} />);
-    //   const button = getByTestId('team-input-box-button');
-    //   fireEvent.click(button);
-    //   expect(onClick).toHaveBeenCalled();
-    // });
-
-    // it('passes the placeholder prop to the input box', () => {
-    //   const { getByPlaceholderText } = render(<TeamInputBox placeholder="Test Placeholder" />);
-    //   const input = getByPlaceholderText('Test Placeholder');
-    //   expect(input).toBeInTheDocument();
-    // });
+  // it('passes the placeholder prop to the input box', () => {
+  //   const { getByPlaceholderText } = render(<TeamInputBox placeholder="Test Placeholder" />);
+  //   const input = getByPlaceholderText('Test Placeholder');
+  //   expect(input).toBeInTheDocument();
+  // });
 });
