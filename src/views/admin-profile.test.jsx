0601255a5f54d81dc63957eb33fb13916bd77f03
Sayfullah <sayfullah.jumoorty@live.com>
2023-04-26 22:44:20 +0200

    Admin Profile test cases

--- src/views/admin-profile.test.jsx
+++ src/views/admin-profile.test.jsx
@@ -1,51 +1,194 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import { BrowserRouter as Router } from "react-router-dom";
 import '@testing-library/jest-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+import axios from 'axios';
+import bycrypt from "bcryptjs";
+import { putUserDetails, checkIfDetailsValid, update, AdminProfile } from './admin-profile';
+import React, { useState } from 'react';
+import InputBoxForInfo from "../components/input-box-for-info";
 
-const { describe, expect, it } = require("@jest/globals");
-const { checkIfDetailsValid, AdminProfile } = require("./admin-profile");
+//jest.mock('axios');
 
-describe("checkIfDetailsValid function", () => {
-    it("should return false when newEmail and newUsername are empty", () => {
-      const result = checkIfDetailsValid("", "", "currentPassword", "newPassword");
-      expect(result).toBe(false);
-    });
+// jest.mock('react', () => ({
+//     ...jest.requireActual('react'),
+//     useState: jest.fn().mockImplementation((initialValue) => [initialValue, jest.fn()]),
+//   }));  
   
-    it("should return false when password is empty", () => {
-      const result = checkIfDetailsValid("newEmail@example.com", "newUsername", "", "newPassword");
-      expect(result).toBe(false);
-    });
+jest.mock('axios', () => ({
+create: jest.fn(() => ({
+    interceptors: {
+    request: { use: jest.fn(), eject: jest.fn() },
+    response: { use: jest.fn(), eject: jest.fn() },
+    },
+})),
+get: jest.fn(),
+post: jest.fn(),
+put: jest.fn(),
+delete: jest.fn(),
+}));
   
-    it("should return false when newPassword is too weak", () => {
-      const result = checkIfDetailsValid("newEmail@example.com", "newUsername", "currentPassword", "weak");
-      expect(result).toBe(false);
-    });
-  
-    it("should return false when newEmail is invalid", () => {
-      const result = checkIfDetailsValid("notAnEmail", "newUsername", "currentPassword", "newPassword");
-      expect(result).toBe(false);
-    });
-  
-    it("should return true when all fields are valid", () => {
-      const result = checkIfDetailsValid("newEmail@example.com", "newUsername", "currentPassword", "newPassword123");
-      expect(result).toBe(true);
-    });
 
-    it("should return true when only newEmail is valid", () => {
-        const result = checkIfDetailsValid("newEmail@example.com", "", "currentPassword", "newPassword123");
-        expect(result).toBe(false);
-      });
-      
-    it("should return true when only newUsername is valid", () => {
-        const result = checkIfDetailsValid("", "newUsername", "currentPassword", "newPassword123");
-        expect(result).toBe(false);
+describe('putUserDetails function', () => {
+  test('should make a POST request to update user details', async () => {
+    axios.post.mockResolvedValueOnce({ data: {} });
+
+    const userID = '1234';
+    const newEmail = 'test@test.com';
+    const newUsername = 'testuser';
+    const newPassword = 'testpassword';
+
+    await putUserDetails(userID, newEmail, newUsername, newPassword);
+
+    expect(axios.post).toHaveBeenCalledWith('http://localhost:3002/api/post/updateDetails', {
+      user_id: userID,
+      user_email: newEmail,
+      user_nickname: newUsername,
+      user_password: newPassword,
     });
-      
-    it("should return false when all fields are invalid", () => {
-        const result = checkIfDetailsValid("notAnEmail", "", "", "weak");
-        expect(result).toBe(false);
+  });
+});
+
+describe('checkIfDetailsValid function', () => {
+  test('should return true when all fields are valid', () => {
+    const newEmail = 'test@test.com';
+    const newUsername = 'testuser';
+    const password = 'testpassword';
+    const newPassword = 'TestPassword123';
+
+    expect(checkIfDetailsValid(newEmail, newUsername, password, newPassword)).toBe(true);
+  });
+
+  test('should return false when email or username is empty', () => {
+    const newEmail = '';
+    const newUsername = 'testuser';
+    const password = 'testpassword';
+    const newPassword = 'TestPassword123';
+
+    expect(checkIfDetailsValid(newEmail, newUsername, password, newPassword)).toBe(false);
+  });
+
+  test('should return false when current password is empty', () => {
+    const newEmail = 'test@test.com';
+    const newUsername = 'testuser';
+    const password = '';
+    const newPassword = 'TestPassword123';
+
+    expect(checkIfDetailsValid(newEmail, newUsername, password, newPassword)).toBe(false);
+  });
+
+  test('should return false when email is not in correct format', () => {
+    const newEmail = 'invalid-email-format';
+    const newUsername = 'testuser';
+    const password = 'testpassword';
+    const newPassword = 'TestPassword123';
+
+    expect(checkIfDetailsValid(newEmail, newUsername, password, newPassword)).toBe(false);
+  });
+
+  test('should return false when password is not in correct format', () => {
+    const newEmail = 'test@test.com';
+    const newUsername = 'testuser';
+    const password = 'testpassword';
+    const newPassword = 'weakpassword';
+
+    expect(checkIfDetailsValid(newEmail, newUsername, password, newPassword)).toBe(false);
+  });
+});
+
+describe('update function', () => {
+    beforeEach(() => {
+      // Clear local storage before each test
+      localStorage.clear();
+    });
+  
+    test('should update user details when old password is correct and new password is not changed', async () => {
+      // Arrange
+      const userID = 123;
+      const newEmail = 'newemail@example.com';
+      const newUsername = 'newusername';
+      const oldPassword = 'oldpassword';
+      const password = oldPassword;
+      const newPassword = '';
+      const confirmPassword = '';
+      const mockCompare = jest.fn((pass, hash, callback) => callback(null, true));
+      const mockHashSync = jest.fn((pass, salt) => pass);
+      bycrypt.compare = mockCompare;
+      bycrypt.hashSync = mockHashSync;
+      await putUserDetails(userID, newEmail, newUsername, newPassword);
+      window.location.href = jest.fn();
+  
+      // Act
+      update(password, oldPassword, newPassword, confirmPassword, userID, newEmail, newUsername);
+      //await screen.findByText('Email and username updated');
+  
+      // Assert
+      expect(mockCompare).toHaveBeenCalledWith(password, oldPassword, expect.any(Function));
+      expect(mockHashSync).not.toHaveBeenCalled();
+      //expect(putUserDetails).toHaveBeenCalledWith(userID, newEmail, newUsername, oldPassword);
+      //expect(window.location.href).toHaveBeenCalledWith('http://localhost:3000/arena-home');
+    });
+  
+    test('should update user details when old password is correct and new password is changed', async () => {
+      // Arrange
+      const userID = 123;
+      const newEmail = 'newemail@example.com';
+      const newUsername = 'newusername';
+      const oldPassword = 'oldpassword';
+      const password = oldPassword;
+      const newPassword = 'newpassword123';
+      const confirmPassword = newPassword;
+      const mockCompare = jest.fn((pass, hash, callback) => callback(null, true));
+      const mockHashSync = jest.fn((pass, salt) => `hashed-${pass}`);
+      bycrypt.compare = mockCompare;
+      bycrypt.hashSync = mockHashSync;
+      await putUserDetails(userID, newEmail, newUsername, newPassword);
+      window.location.href = jest.fn();
+  
+      // Act
+      update(password, oldPassword, newPassword, confirmPassword, userID, newEmail, newUsername);
+      //await screen.findByText('Details updated');
+  
+      // Assert
+      expect(mockCompare).toHaveBeenCalledWith(password, oldPassword, expect.any(Function));
+      expect(mockHashSync).not.toHaveBeenCalled();
+      //expect(putUserDetails).toHaveBeenCalledWith(userID, newEmail, newUsername, `hashed-${newPassword}`);
+      //expect(window.location.href).toHaveBeenCalledWith('http://localhost:3000/arena-home');
     });
 });
+
+// describe('InputBoxForInfo', () => {
+//     test('updates new email when input field changes', () => {
+//       // Arrange
+//       const initialValue = '';
+//       const buttonText = 'EMAIL';
+//       const setIsLoading = jest.fn();
+//       const setNewUsername = jest.fn();
+//       const setPassword = jest.fn();
+//       const setNewPassword = jest.fn();
+//       const setConfirmPassword = jest.fn();
+//       const setNewEmail = jest.fn();
+//       useState.mockImplementation((initialValue) => [initialValue, setNewEmail]);
+//       render(
+//         <InputBoxForInfo
+//           initialValue={initialValue}
+//           buttonText={buttonText}
+//           setIsLoading={setIsLoading}
+//           setNewUsername={setNewUsername}
+//           setPassword={setPassword}
+//           setNewPassword={setNewPassword}
+//           setConfirmPassword={setConfirmPassword}
+//         />
+//       );
+//       const newEmailInput = screen.getByLabelText('EMAIL');
+  
+//       // Act
+//       fireEvent.change(newEmailInput, { target: { value: 'test@test.com' } });
+  
+//       // Assert that the new email has been updated
+//         expect(newEmailInput.value).toBe('test@test.com');
+
+//     });
+//   });
 
 describe('AdminProfile', () => {
     test('renders navbar', () => {
@@ -59,6 +202,13 @@ describe('AdminProfile', () => {
       const updateProfileSection = screen.getByText('UPDATE PROFILE');
       expect(updateProfileSection).toBeInTheDocument();
     });
+
+    test('updates email when input field changes', () => {
+        render(<Router> <AdminProfile /> </Router>);
+        const emailInput = screen.getByLabelText('USERNAME');
+        fireEvent.change(emailInput, { target: { value: 'Namely' } });
+        expect(emailInput.value).toBe('Namely');
+      });
   
     test('updates email when input field changes', () => {
       render(<Router> <AdminProfile /> </Router>);
@@ -69,14 +219,14 @@ describe('AdminProfile', () => {
   
     test('updates password when input field changes', () => {
       render(<Router> <AdminProfile /> </Router>);
-      const passwordInput = screen.getByLabelText('Current PASSWORD');
+      const passwordInput = screen.getByLabelText('CURRENT PASSWORD');
       fireEvent.change(passwordInput, { target: { value: 'oldpassword' } });
       expect(passwordInput.value).toBe('oldpassword');
     });
   
     test('updates new password when input field changes', () => {
       render(<Router> <AdminProfile /> </Router>);
-      const newPasswordInput = screen.getByLabelText('PASSWORD');
+      const newPasswordInput = screen.getByLabelText('NEW PASSWORD');
       fireEvent.change(newPasswordInput, { target: { value: 'newpassword' } });
       expect(newPasswordInput.value).toBe('newpassword');
     });
@@ -88,4 +238,3 @@ describe('AdminProfile', () => {
       expect(confirmPasswordInput.value).toBe('newpassword');
     });
   });
-  
