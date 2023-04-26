dadcfcb698b43685fd3939013a69437021310950
Sayfullah <sayfullah.jumoorty@live.com>
2023-04-26 22:32:06 +0200

    Arena Profile test cases

--- src/views/arena-profile.js
+++ src/views/arena-profile.js
@@ -1,43 +1,45 @@
-import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import bycrypt from 'bcryptjs';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import bycrypt from "bcryptjs";
+import axios from "axios";
 
 import InputBoxForInfo from "../components/input-box-for-info";
 import Button from "../components/button";
-import './arena-profile.css';
+import "./arena-profile.css";
 
 // Update user details in database
-function putUserDetails (userID, newEmail, newUsername, newPassword) {
-  axios
-    .post("http://localhost:3002/api/post/updateDetails", 
-    {user_id: userID, user_email: newEmail, user_nickname: newUsername, user_password: newPassword});
+async function putUserDetails(userID, newEmail, newUsername, newPassword) {
+  await axios.post("http://localhost:3002/api/post/updateDetails", {
+    user_id: userID,
+    user_email: newEmail,
+    user_nickname: newUsername,
+    user_password: newPassword,
+  });
 }
 
 // Ensures all detail fields are valid
-function checkIfDetailsValid (newEmail, newUsername, password, newPassword) {
+function checkIfDetailsValid(newEmail, newUsername, password, newPassword) {
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 
   //If fields empty, warn
-  if(newEmail == "" || newUsername == ""){
+  if (newEmail == "" || newUsername == "") {
     alert("Email and Username cannot be empty");
     return false;
-  }
-  else if (password == "") {
-    alert("Current Password Required")
+  } else if (password == "") {
+    alert("Current Password Required");
     return false;
   }
 
   //not empty
   else if (newEmail != "" || newUsername != "" || password != "") {
     //email not corect format
-    if(!emailPattern.test(newEmail)){
+    if (!emailPattern.test(newEmail)) {
       alert("Please enter a valid email");
       return false;
     }
     //password not correct format
-    if(!passwordPattern.test(newPassword) && newPassword != ""){
+    if (!passwordPattern.test(newPassword) && newPassword != "") {
       alert("Please enter a stronger password");
       return false;
     }
@@ -46,29 +48,49 @@ function checkIfDetailsValid (newEmail, newUsername, password, newPassword) {
 }
 
 // Handles different update cases
-function update (password, oldPassword, newPassword, confirmPassword, userID, newEmail, newUsername) {
+function update(
+  password,
+  oldPassword,
+  newPassword,
+  confirmPassword,
+  userID,
+  newEmail,
+  newUsername
+) {
   bycrypt.compare(password, oldPassword, function (error, isMatch) {
     if (isMatch) {
       // Password not changed
-      if ((newPassword == confirmPassword) && (newPassword == "" && confirmPassword == "") && checkIfDetailsValid(newEmail, newUsername, password, newPassword)) {
+      if (
+        newPassword == confirmPassword &&
+        newPassword == "" &&
+        confirmPassword == "" &&
+        checkIfDetailsValid(newEmail, newUsername, password, newPassword)
+      ) {
         putUserDetails(userID, newEmail, newUsername, oldPassword);
         alert("Email and username updated");
-        window.location.href = 'http://localhost:3000/arena-home'
+        window.location.href = "http://localhost:3000/arena-home";
       }
-      // Password changed and new passwords matches confirmed password 
-      else if ((newPassword == confirmPassword) && (newPassword != "" && confirmPassword != "") && checkIfDetailsValid(newEmail, newUsername, password, newPassword)) {
+      // Password changed and new passwords matches confirmed password
+      else if (
+        newPassword == confirmPassword &&
+        newPassword != "" &&
+        confirmPassword != "" &&
+        checkIfDetailsValid(newEmail, newUsername, password, newPassword)
+      ) {
         const salt = bycrypt.genSaltSync(10);
-        let hashedNewPassword = bycrypt.hashSync(newPassword,salt);
+        let hashedNewPassword = bycrypt.hashSync(newPassword, salt);
         putUserDetails(userID, newEmail, newUsername, hashedNewPassword);
-        alert("Details updated")
-        window.location.href = 'http://localhost:3000/arena-home'
-      } 
+        alert("Details updated");
+        window.location.href = "http://localhost:3000/arena-home";
+      }
       // New password doesn't match confirmed password
-      else if ((newPassword != confirmPassword) && checkIfDetailsValid(newEmail, newUsername, password, newPassword)){
+      else if (
+        newPassword != confirmPassword &&
+        checkIfDetailsValid(newEmail, newUsername, password, newPassword)
+      ) {
         alert("Passwords do not match");
       }
-    }
-    else {
+    } else {
       if (checkIfDetailsValid(newEmail, newUsername, password, newPassword)) {
         alert("Incorrect Current Password");
       }
@@ -78,38 +100,43 @@ function update (password, oldPassword, newPassword, confirmPassword, userID, ne
 
 const ArenaProfile = (props) => {
   // Get the username, userID, userpassword and useremail from local storage
-  const userID = sessionStorage.getItem('userID');
-  const username = sessionStorage.getItem('username');
-  const email = sessionStorage.getItem('useremail');
-  const oldPassword = sessionStorage.getItem('userpassword')
+  const userID = sessionStorage.getItem("userID");
+  const username = sessionStorage.getItem("username");
+  const email = sessionStorage.getItem("useremail");
+  const oldPassword = sessionStorage.getItem("userpassword");
   let [password, setPassword] = useState("");
   let [newPassword, setNewPassword] = useState("");
   let [confirmPassword, setConfirmPassword] = useState("");
   let [newUsername, setNewUsername] = useState("");
   let [newEmail, setNewEmail] = useState("");
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    update(password, oldPassword, newPassword, confirmPassword, userID, newEmail, newUsername);
+    update(
+      password,
+      oldPassword,
+      newPassword,
+      confirmPassword,
+      userID,
+      newEmail,
+      newUsername
+    );
   };
-  
+
   // Get user_email from database
   const setUserDetails = () => {
-        setNewUsername(username);
-        setNewEmail(email);
-  }
+    setNewUsername(username);
+    setNewEmail(email);
+  };
 
   // Allows current details to display when page loads
   useEffect(() => {
-    setUserDetails()
-  }, []); 
+    setUserDetails();
+  }, []);
 
   return (
     <div className="arena-profile-container">
-      <div
-        data-role="Header"
-        className="arena-profile-navbar-container"
-      >
+      <div role="Header" className="arena-profile-navbar-container">
         <div className="arena-profile-navbar">
           <div className="arena-profile-left-side">
             <img
@@ -117,30 +144,27 @@ const ArenaProfile = (props) => {
               src="https://play.teleporthq.io/static/svg/default-img.svg"
               className="arena-profile-image"
             />
-            <div
-              data-role="BurgerMenu"
-              className="arena-profile-burger-menu"
-            >
-              <svg
-                viewBox="0 0 1024 1024"
-                className="arena-profile-icon"
-              >
+            <div data-role="BurgerMenu" className="arena-profile-burger-menu">
+              <svg viewBox="0 0 1024 1024" className="arena-profile-icon">
                 <path d="M128 256h768v86h-768v-86zM128 554v-84h768v84h-768zM128 768v-86h768v86h-768z"></path>
               </svg>
             </div>
-              <Link to="/player-portal-competitions" className="arena-main-link">
-                  <svg viewBox="0 0 1024 1024" className="arena-main-icon2">
-                      <path d="M896 470v84h-604l152 154-60 60-256-256 256-256 60 60-152 154h604z"></path>
-                  </svg>
-              </Link>
+            <Link to="/player-portal-competitions" className="arena-main-link">
+              <svg viewBox="0 0 1024 1024" className="arena-main-icon2">
+                <path d="M896 470v84h-604l152 154-60 60-256-256 256-256 60 60-152 154h604z"></path>
+              </svg>
+            </Link>
             <div className="arena-profile-links-container">
               <Link to="/arena-main" className="arena-main-link">
-                  ARENA
+                ARENA
               </Link>
               <Link to="/arena-submissions" className="arena-profile-link">
                 SUBMISSIONS
               </Link>
-              <Link to="/arena-leaderboard" className="arena-profile-link1 Anchor">
+              <Link
+                to="/arena-leaderboard"
+                className="arena-profile-link1 Anchor"
+              >
                 LEADERBOARD
               </Link>
               <Link to="/arena-team" className="arena-profile-link2 Anchor">
@@ -150,18 +174,12 @@ const ArenaProfile = (props) => {
           </div>
           <div className="arena-profile-container1">
             <Link to="/arena-profile" className="arena-profile-navlink">
-              <svg
-                viewBox="0 0 1024 1024"
-                className="arena-profile-icon2"
-              >
+              <svg viewBox="0 0 1024 1024" className="arena-profile-icon2">
                 <path d="M576 706.612v-52.78c70.498-39.728 128-138.772 128-237.832 0-159.058 0-288-192-288s-192 128.942-192 288c0 99.060 57.502 198.104 128 237.832v52.78c-217.102 17.748-384 124.42-384 253.388h896c0-128.968-166.898-235.64-384-253.388z"></path>
               </svg>
             </Link>
           </div>
-          <div
-            data-role="MobileMenu"
-            className="arena-profile-mobile-menu"
-          >
+          <div data-role="MobileMenu" className="arena-profile-mobile-menu">
             <div className="arena-profile-container2">
               <img
                 alt="image"
@@ -172,10 +190,7 @@ const ArenaProfile = (props) => {
                 data-role="CloseMobileMenu"
                 className="arena-profile-close-menu"
               >
-                <svg
-                  viewBox="0 0 1024 1024"
-                  className="arena-profile-icon4"
-                >
+                <svg viewBox="0 0 1024 1024" className="arena-profile-icon4">
                   <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
                 </svg>
               </div>
@@ -184,7 +199,10 @@ const ArenaProfile = (props) => {
               <Link to="/arena-submissions" className="arena-profile-link">
                 SUBMISSIONS
               </Link>
-              <Link to="/arena-leaderboard" className="arena-profile-link1 Anchor">
+              <Link
+                to="/arena-leaderboard"
+                className="arena-profile-link1 Anchor"
+              >
                 LEADERBOARD
               </Link>
               <Link to="/arena-team" className="arena-profile-link2 Anchor">
@@ -204,55 +222,58 @@ const ArenaProfile = (props) => {
           initialValue={username}
           buttonText="USERNAME"
           onChange={(e) => setNewUsername(e.target.value)}
-        >
-        </InputBoxForInfo>
+        ></InputBoxForInfo>
         <InputBoxForInfo
           initialValue={email}
           buttonText="EMAIL"
           onChange={(e) => setNewEmail(e.target.value)}
-        >
-        </InputBoxForInfo>
-        <InputBoxForInfo 
-          buttonText="CURRENT PASSWORD" 
+        ></InputBoxForInfo>
+        <InputBoxForInfo
+          buttonText="CURRENT PASSWORD"
           isPassword
           onChange={(e) => setPassword(e.target.value)}
-        >
-        </InputBoxForInfo>
-        <InputBoxForInfo 
-          buttonText="NEW PASSWORD" 
+        ></InputBoxForInfo>
+        <InputBoxForInfo
+          buttonText="NEW PASSWORD"
           isPassword
           onChange={(e) => setNewPassword(e.target.value)}
-        >
-        </InputBoxForInfo>
-        <InputBoxForInfo 
-          buttonText="CONFIRM PASSWORD" 
+        ></InputBoxForInfo>
+        <InputBoxForInfo
+          buttonText="CONFIRM PASSWORD"
           isPassword
           onChange={(e) => setConfirmPassword(e.target.value)}
-        >
-        </InputBoxForInfo>
+        ></InputBoxForInfo>
         <Button
-          type = "submit"
+          type="submit"
           name="UPDATE"
           onClick={() => {
             console.log("Register button clicked");
-            update(password, oldPassword, newPassword, confirmPassword, userID, newEmail, newUsername);
+            update(
+              password,
+              oldPassword,
+              newPassword,
+              confirmPassword,
+              userID,
+              newEmail,
+              newUsername
+            );
           }}
           rootClassName="button-root-class-name4"
         ></Button>
         <br></br>
-         <Button
+        <Button
           name="LOG OUT"
           onClick={() => {
             console.log("Log out button clicked");
             sessionStorage.clear();
-            window.location.href = 'http://localhost:3000/login'
+            window.location.href = "http://localhost:3000/login";
           }}
           rootClassName="button-root-class-name4"
         ></Button>
         <div className="arena-profile-container4"></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ArenaProfile
+export { ArenaProfile, update, putUserDetails, checkIfDetailsValid };
