681a3299fa0aced5c383581602825846c9d2ed0e
Muhammed <momosuli8@gmail.com>
2023-05-18 20:37:06 +0200

    Tabs test

--- src/components/tabs.test.jsx
+++ src/components/tabs.test.jsx
@@ -1,242 +1,68 @@
-import React from "react";
-import { render, fireEvent } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import BasicTabs from "./tabs";
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import BasicTabs from './tabs';
+import '@testing-library/jest-dom';
 
-describe("BasicTabs", () => {
-  const onSubmitMock = jest.fn();
+describe('BasicTabs', () => {
+  const mockSubmit = jest.fn();
+  const mockLabels = ['Tab 1', 'Tab 2', 'Tab 3'];
+  const mockTabCount = 3;
+  const mockTabContent = ['Content 1', 'Content 2', 'Content 3'];
+  const mockTabContent2 = ['Content 1', 'Content 2', 'Content 3'];
 
-  //The component renders the correct number of tabs based on the tabCount prop.
-  test("renders correct number of tabs", () => {
-    const tabCount = 3;
-    const tabContent = [
-      <div key="tab1">Tab 1 Content</div>,
-      <div key="tab2">Tab 2 Content</div>,
-      <div key="tab3">Tab 3 Content</div>,
-    ];
+  test('renders tabs with correct labels', () => {
+    const { getByText } = render(
+      <BasicTabs
+        onSubmit={mockSubmit}
+        labels={mockLabels}
+        tabCount={mockTabCount}
+        tabContent={mockTabContent}
+        tabContent2={mockTabContent2}
+      />
+    );
 
+    // Check that each label is rendered in the component
+    mockLabels.forEach((label) => {
+      expect(getByText(label)).toBeInTheDocument();
+    });
+  });
+
+  test('calls onSubmit with correct index when button is clicked', () => {
+    const { getByText } = render(
+      <BasicTabs
+        onSubmit={mockSubmit}
+        labels={mockLabels}
+        tabCount={mockTabCount}
+        tabContent={mockTabContent}
+        tabContent2={mockTabContent2}
+      />
+    );
+
+    const button = getByText('Upload Solution');
+    fireEvent.click(button);
+
+    // Check that the onSubmit function is called with the correct index value
+    expect(mockSubmit).toHaveBeenCalledTimes(1);
+    expect(mockSubmit).toHaveBeenCalledWith(0); // Assuming the index value for the first tab is 0
+  });
+
+  test('updates value state when handleChange is called', () => {
     const { getAllByRole } = render(
       <BasicTabs
-        onSubmit={onSubmitMock}
-        tabCount={tabCount}
-        tabContent={tabContent}
+        onSubmit={mockSubmit}
+        labels={mockLabels}
+        tabCount={mockTabCount}
+        tabContent={mockTabContent}
+        tabContent2={mockTabContent2}
       />
     );
 
-    const tabs = getAllByRole("tab");
-    expect(tabs.length).toBe(tabCount);
+    const tabs = getAllByRole('tab'); // Get all the tab elements
+    const secondTab = tabs[1]; // Assuming you want to test the second tab
+
+    fireEvent.click(secondTab);
+
+    // Check that the second tab has the expected attribute value
+    expect(secondTab).toHaveAttribute('aria-selected', 'true');
   });
-
-  // The component calls the onSubmit prop when the submit button is clicked.
-  // Clicking the submit button in a tab calls the onSubmit prop with the index of the current tab.
-  test("calls onSubmit prop when submit button is clicked", () => {
-    const tabCount = 1;
-    const tabContent = [<div key="tab1">Tab 1 Content</div>];
-
-    const { getByRole } = render(
-      <BasicTabs
-        onSubmit={onSubmitMock}
-        tabCount={tabCount}
-        tabContent={tabContent}
-      />
-    );
-
-    fireEvent.click(getByRole("button"));
-    expect(onSubmitMock).toHaveBeenCalledTimes(1);
-    expect(onSubmitMock).toHaveBeenCalledWith(0); // should call with index of current tab (0 in this case)
-  });
-
-  it("should render tabs and content correctly", () => {
-    const onSubmitMock = jest.fn();
-    const tabCount = 3;
-    const tabContent = [10, 5, 8];
-    const { getByText } = render(
-      <BasicTabs
-        onSubmit={onSubmitMock}
-        tabCount={tabCount}
-        tabContent={tabContent}
-      />
-    );
-
-    // Check if all the tabs are rendered with the correct labels
-    expect(getByText("Testcase 1")).toBeInTheDocument();
-    expect(getByText("Testcase 2")).toBeInTheDocument();
-    expect(getByText("Testcase 3")).toBeInTheDocument();
-
-    // Check if the first tab content is rendered correctly
-    expect(getByText("Score: 10")).toBeInTheDocument();
-
-    // Click on the second tab and check if its content is rendered correctly
-    fireEvent.click(getByText("Testcase 2"));
-    expect(getByText("Score: 5")).toBeInTheDocument();
-
-    // Click on the third tab and check if its content is rendered correctly
-    fireEvent.click(getByText("Testcase 3"));
-    expect(getByText("Score: 8")).toBeInTheDocument();
-
-    // Click on the "Upload Solution" button for the third tab and check if the onSubmit function is called with the correct index
-    fireEvent.click(getByText("Upload Solution"));
-    expect(onSubmitMock).toHaveBeenCalledWith(2);
-  });
-
-  it("should render tabs and content correctly", () => {
-    const onSubmitMock = jest.fn();
-    const tabCount = 3;
-    const tabContent = [10, 5, 8];
-    const { getByText } = render(
-      <BasicTabs
-        onSubmit={onSubmitMock}
-        tabCount={tabCount}
-        tabContent={tabContent}
-      />
-    );
-
-    // Check if all the tabs are rendered with the correct labels
-    expect(getByText("Testcase 1")).toBeInTheDocument();
-    expect(getByText("Testcase 2")).toBeInTheDocument();
-    expect(getByText("Testcase 3")).toBeInTheDocument();
-
-    // Check if the first tab content is rendered correctly
-    expect(getByText("Score: 10")).toBeInTheDocument();
-
-    // Click on the second tab and check if its content is rendered correctly
-    fireEvent.click(getByText("Testcase 2"));
-    expect(getByText("Score: 5")).toBeInTheDocument();
-
-    // Click on the third tab and check if its content is rendered correctly
-    fireEvent.click(getByText("Testcase 3"));
-    expect(getByText("Score: 8")).toBeInTheDocument();
-
-    // Click on the "Upload Solution" button for the third tab and check if the onSubmit function is called with the correct index
-    fireEvent.click(getByText("Upload Solution"));
-    expect(onSubmitMock).toHaveBeenCalledWith(2);
-  });
-
-  it("should render the first tab by default", () => {
-    const onSubmitMock = jest.fn();
-    const tabCount = 2;
-    const tabContent = [10, 8];
-    const { getByText } = render(
-      <BasicTabs
-        onSubmit={onSubmitMock}
-        tabCount={tabCount}
-        tabContent={tabContent}
-      />
-    );
-    expect(getByText("Score: 10")).toBeInTheDocument();
-  });
-
-  it("should switch between tabs on click", () => {
-    const onSubmitMock = jest.fn();
-    const tabCount = 2;
-    const tabContent = [10, 8];
-    const { getByText } = render(
-      <BasicTabs
-        onSubmit={onSubmitMock}
-        tabCount={tabCount}
-        tabContent={tabContent}
-      />
-    );
-
-    fireEvent.click(getByText("Testcase 2"));
-    expect(getByText("Score: 8")).toBeInTheDocument();
-
-    fireEvent.click(getByText("Testcase 1"));
-    expect(getByText("Score: 10")).toBeInTheDocument();
-  });
-
-  it("should call the onSubmit function when Upload Solution button is clicked", () => {
-    const onSubmitMock = jest.fn();
-    const tabCount = 1;
-    const tabContent = [5];
-    const { getByText } = render(
-      <BasicTabs
-        onSubmit={onSubmitMock}
-        tabCount={tabCount}
-        tabContent={tabContent}
-      />
-    );
-
-    fireEvent.click(getByText("Upload Solution"));
-    expect(onSubmitMock).toHaveBeenCalledWith(0);
-  });
-
-  // test('shows the correct tab as active', () => {
-  //   const tabCount = 3;
-  //   const tabContent = [
-  //     <div key="tab1">Tab 1 Content</div>,
-  //     <div key="tab2">Tab 2 Content</div>,
-  //     <div key="tab3">Tab 3 Content</div>,
-  //   ];
-
-  //   const { getByText } = render(
-  //     <BasicTabs
-  //       onSubmit={onSubmitMock}
-  //       tabCount={tabCount}
-  //       tabContent={tabContent}
-  //     />
-  //   );
-
-  //   expect(getByText('Tab 1 Content')).toBeInTheDocument();
-  //   expect(getByText('Testcase 1')).toHaveAttribute('aria-selected', 'true');
-
-  //   fireEvent.click(getByText('Testcase 2'));
-  //   expect(getByText('Tab 2 Content')).toBeInTheDocument();
-  //   expect(getByText('Testcase 2')).toHaveAttribute('aria-selected', 'true');
-
-  //   fireEvent.click(getByText('Testcase 3'));
-  //   expect(getByText('Tab 3 Content')).toBeInTheDocument();
-  //   expect(getByText('Testcase 3')).toHaveAttribute('aria-selected', 'true');
-  // });
-
-  // test('displays only one tabpanel at a time', () => {
-  //   const tabCount = 3;
-  //   const tabContent = [
-  //     <div key="tab1">Tab 1 Content</div>,
-  //     <div key="tab2">Tab 2 Content</div>,
-  //     <div key="tab3">Tab 3 Content</div>,
-  //   ];
-
-  //   const { getByText } = render(
-  //     <BasicTabs
-  //       onSubmit={onSubmitMock}
-  //       tabCount={tabCount}
-  //       tabContent={tabContent}
-  //     />
-  //   );
-
-  //   fireEvent.click(getByText('Testcase 2'));
-  //   expect(getByText('Tab 2 Content')).toBeInTheDocument();
-  //   // expect(getByText('Tab 1 Content')).not.toBeVisible();
-  //   // expect(getByText('Tab 3 Content')).not.toBeVisible();
-
-  //   fireEvent.click(getByText('Testcase 3'));
-  //   expect(getByText('Tab 3 Content')).toBeInTheDocument();
-  //   // expect(getByText('Tab 1 Content')).not.toBeVisible();
-  //   // expect(getByText('Tab 2 Content')).not.toBeVisible();
-  // });
-
-  // The component renders the correct content based on the tabContent prop.
-  // Clicking a tab shows the corresponding content from the tabContent prop.
-  // test('renders correct content when tab is clicked', () => {
-  //   const tabCount = 2;
-  //   const tabContent = [
-  //     <div key="tab1">Tab 1 Content</div>,
-  //     <div key="tab2">Tab 2 Content</div>,
-  //   ];
-
-  //   const { getByText } = render(
-  //     <BasicTabs
-  //       onSubmit={onSubmitMock}
-  //       tabCount={tabCount}
-  //       tabContent={tabContent}
-  //     />
-  //   );
-
-  //   fireEvent.click(getByText('Testcase 2'));
-  //   expect(getByText('Tab 2 Content')).toBeInTheDocument();
-
-  //   fireEvent.click(getByText('Testcase 1'));
-  //   expect(getByText('Tab 1 Content')).toBeInTheDocument();
-  // });
 });
