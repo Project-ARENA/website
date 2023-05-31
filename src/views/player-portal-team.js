6dae3192a9586e845285039dde4cfb049ad0987e
Muhammed <momosuli8@gmail.com>
2023-05-31 22:11:44 +0200

    Uploads submission links

--- src/views/player-portal-team.js
+++ src/views/player-portal-team.js
@@ -166,6 +166,7 @@ const PlayerPortalTeam = (props) => {
       testcase_latest: createJsonArray(no_testcases),
       testcase_highest: createJsonArray(no_testcases),
       team_name: teamName,
+      testcase_links : createJsonArray(no_testcases)
     });
     axios.post("http://localhost:3002/api/post/addTo/team", {
       user_id: parseInt(userID),
